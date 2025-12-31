eccacb67c4c0d18cb09182d1beb1447838b5fd79
4zazel <michal.sztaba@op.pl>
2025-12-31 01:07:33 +0100

    progress

--- ToDo.md
+++ ToDo.md
@@ -1,19 +1,33 @@
 
 ## **GAME IDEA** 
-### - Simple top down shooter 
-    - Move around
-    - Have some kind of attack
-    - Have some kind of enemies
-    - Have some kind of goal
-    - Have some kind of point system
+### - Wave based top down shooter
+~~Move around~~
+~~Look around~~ 
+Have some kind of attack
+Have some kind of enemies
+Have some kind of goal
+Have some kind of point system 
+Have some kind of stats
+After getting enough points get to pick an upgrade (speed, dmg, health)
 
 #### Urgent
-  - [ ] game idea xddd
-  - [ ] movement
-  - [ ] collisions
+  - [x] game idea xddd
+  - [x] movement
+
 # Need to do
+  - [ ] shooting
+  - [ ] enemies
+    - [ ] Add enemies
+    - [ ] Make them spawn in waves
+  - [ ] upgrades
+  - [ ] health
+  - [ ] win condition
 
 ###### Nice to have
+1. Unlockables
+  - [ ] Characters
+  - [ ] Weapons
+- [ ] Levels
 
 ## Done
 - [x] Player rotation based on mouse
--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 #include <cmath>
 #include <iostream>
 
+void player_movement();
+void player_rotation();
+void player_shoot();
+
+Rectangle player;
+int p_rotation = 0;
+int deltaX;
+int deltaY;
+Color player_color = RED;
+
 int main(void)
 {
   const int screenWidth{ 1920 };
@@ -12,37 +22,48 @@ int main(void)
   SetTargetFPS(60);
   //ToggleFullscreen();
 
-  Rectangle player;
   player.height = 50;
   player.width = 50;
   player.x = static_cast<float>(screenWidth)/2;
   player.y = static_cast<float>(screenHeight)/2;
 
-  int player_rotation = 0;
-
-  int deltaX;
-  int deltaY;
-
   while(!WindowShouldClose()) {
 
-    deltaX = player.x - GetMousePosition().x;
-    deltaY = player.y - GetMousePosition().y;
-
-    player_rotation = (std::atan2(-deltaX, deltaY) * 180.000) / 3.141592;
-    std::cout<<player_rotation<<std::endl;
-
-    if(IsKeyDown(KEY_D)) player.x+=5; 
-    if(IsKeyDown(KEY_A)) player.x-=5;
-    if(IsKeyDown(KEY_W)) player.y-=5;
-    if(IsKeyDown(KEY_S)) player.y+=5;
+    player_movement();
+    player_rotation();
+    player_shoot();
 
     BeginDrawing();
       ClearBackground(BLACK);
-      DrawRectanglePro(player, {player.height/2, player.width/2}, player_rotation, RED);
+      DrawRectanglePro(player, {player.height/2, player.width/2}, p_rotation, player_color);
     EndDrawing();
   }
 
   CloseWindow();
 
   return 0;
+}
+
+void player_movement()
+{
+    if(IsKeyDown(KEY_D)) player.x+=5;
+    if(IsKeyDown(KEY_A)) player.x-=5;
+    if(IsKeyDown(KEY_W)) player.y-=5;
+    if(IsKeyDown(KEY_S)) player.y+=5;
+
+    std::cout<<"TEST"<<std::endl;
+}
+
+void player_rotation()
+{
+    deltaX = player.x - GetMousePosition().x;
+    deltaY = player.y - GetMousePosition().y;
+
+    p_rotation = (std::atan2(-deltaX, deltaY) * 180.000) / 3.141592;
+}
+
+void player_shoot()
+{
+    if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) player_color = WHITE;
+    if(IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) player_color = RED;
 }
